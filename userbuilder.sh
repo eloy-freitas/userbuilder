577984ccaccc36d0df35f3a5ebed56742d1aae08
eloy <freitaseloy.1@gmail.com>
2020-07-31 15:29:49 -0300

    script de criação de grupos e usuários

---
+++ userbuilder.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -n "enter the group name: "
+read group
+
+GROUPNAME=`getent group $group`
+while test ! -z $GROUPNAME; do
+    echo "this group already exist, try another."
+    echo -n "enter the group name: "
+    read group
+    GROUPNAME=`getent group $group`
+
+done
+echo "$group"
+
+echo -n "enter the name of the user: "
+read user
+
+USERNAME=`getent passwd $user`
+while test ! -z $USERNAME; do
+    echo "this username already exist, try another."
+    echo -n "enter the name of the user: "
+    read user
+    USERNAME=`getent passwd $user`
+done
+echo "useradd -b /bin/bash -g $group -b / -d $user $user"
+
+
+#groupadd $group
